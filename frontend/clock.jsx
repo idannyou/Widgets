d96931c4928ffc30347c77ee8b91bd88d4b92159
Danny Ou <idannyou@gmail.com>
2017-05-11 15:46:47 -0400

    Add tick function to set Date

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 
 class Clock extends React.Component{
 
+  constructor(){
+    super();
+    this.state = {
+      time: new Date()
+    };
+  }
+
+  tick(){
+    this.setState({time: new Date()});
+  }
+
   render(){
     return(
       <div>
-        In Clock
+        <h1>Clock</h1>
+        {this.state.time.toString()}
       </div>
     );
   }
