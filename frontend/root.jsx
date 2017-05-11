71d51da7bd04bb522d21507250700e6e060527ba
Danny Ou <idannyou@gmail.com>
2017-05-11 15:01:43 -0400

    Add webpack-dev-server

---
+++ frontend/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Root extends React.Component {
+
+  render(){
+    return(
+      <div>
+        Testing
+      </div>
+    );
+  }
+
+}
+
+export default Root;
