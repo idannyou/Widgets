b31c83c46e1cc76b28081f9cc82244f5cb8c4e8d
Danny Ou <idannyou@gmail.com>
2017-05-11 16:17:20 -0400

    Convert Date object to readable string

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -6,19 +6,42 @@ class Clock extends React.Component{
   constructor(){
     super();
     this.state = {
-      time: new Date()
+      date_time: new Date()
     };
+    this.handle = null;
+  }
+
+  componentDidMount(){
+    this.updateTime();
+  }
+
+  componentWillUnmount(){
+    // setInterval sets up a recurring timer and returns
+    // a handle
+    // clear interval, so setInterval stops running after
+    // DOM unmounts
+    this.clearInterval(this.handle);
+    this.handle = 0;
   }
 
   tick(){
-    this.setState({time: new Date()});
+    this.handle = this.setState({date_time: new Date()});
+  }
+
+  updateTime(){
+    setInterval(() => this.tick(), 1000);
   }
 
   render(){
     return(
       <div>
         <h1>Clock</h1>
-        {this.state.time.toString()}
+        <h2>
+          {this.state.date_time.toDateString()}
+        </h2>
+        <h2>
+          {this.state.date_time.toLocaleTimeString()}
+        </h2>
       </div>
     );
   }
