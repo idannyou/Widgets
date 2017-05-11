2f26b473b334620d8e798ef119571829da6b48d3
Danny Ou <idannyou@gmail.com>
2017-05-11 19:48:44 -0400

    Add buttons in root

--- frontend/root.jsx
+++ frontend/root.jsx
@@ -2,13 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
 
-const Root = () => {
+class Root extends React.Component {
 
-  return(
-    <div>
-      <Clock />
-    </div>
+  constructor(){
+    super();
+    this.state = {
+      children: []
+    };
+
+    this.addClock = this.addClock.bind(this);
+    this.addWeather = this.addWeather.bind(this);
+  }
+
+  addClock(){
+    let newChildren = Object.assign([], this.state.children);
+    newChildren.push(
+      <Clock key={newChildren.length}/>
   );
+    this.setState({children: newChildren});
+  }
+
+  addWeather(){
+    let newChildren = Object.assign([], this.state.children);
+    newChildren.push(<h1 key={newChildren.length}>Weather Testing</h1>);
+    this.setState({children: newChildren});
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.children}
+        <button type='button' onClick={this.addClock}>Add Clock</button>
+        <button type='button' onClick={this.addWeather}>Add Weather</button>
+      </div>
+    );
+  }
 
 };
 
